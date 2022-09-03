8920068b96b381018b1d15e0be7cf8db854b9def
Bert Joseph Prestoza <bertjosephprestoza@gmail.com>
2022-09-02 22:04:28 -0700

    Add library array and form functionality

--- script.js
+++ script.js
@@ -32,3 +32,61 @@ overlay.addEventListener('click', () => {
         closeModal(modal);
     })
 })
+
+
+// add books to library
+
+const form = document.getElementById('form');
+const submitButton = document.querySelector('.submit-button');
+
+let myLibrary = [];
+let newBook;
+let isValid = false;
+
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+function getFormValidity(title, author, pages) {
+    if (title === '' || author === '' || (pages <= 0 || pages === '')) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function getFormInput() {
+    const bookTitle = document.getElementById('title').value;
+    const bookAuthor = document.getElementById('author').value;
+    const bookPages = document.getElementById('pages').value;
+    const bookIsRead = document.getElementById('isRead').checked;
+    isValid = getFormValidity(bookTitle, bookAuthor, bookPages);
+    return new Book(bookTitle, bookAuthor, bookPages, bookIsRead);
+}
+
+function addBookToLibrary() {
+    newBook = getFormInput();
+    myLibrary.push(newBook);
+}
+
+function resetForm() {
+    form.reset();
+    isValid = false;
+}
+
+
+// add books after submitting form
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    getFormInput();
+
+    if (isValid) {
+        addBookToLibrary();
+        resetForm();
+        closeModal(modal);
+    }
+})
